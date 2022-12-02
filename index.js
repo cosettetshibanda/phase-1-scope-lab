22258372a4b487a9172d4bba9df5a05b592f86e6
Cosette Tshibanda <cosettetshibanda@gmail.com>
2022-12-01 20:59:50 -0700

    commit tree 575263d9a105753d51049d4f6df0bc80effadf90

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+   return bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'Tom'
+function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer = 'Fred'
+}
